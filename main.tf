fe951235cac8c5b27a4b3c799a4eceae13d3b1c8
Patrick Cowland <patrickcowland@pingidentity.com>
2022-07-02 18:37:34 +0100

    initial example

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    pingone = {
+      source  = "pingidentity/pingone"
+      version = "~> 0.1"
+    }
+  }
+}
+
+provider "pingone" {
+  client_id      = var.p1_adminClientId
+  client_secret  = var.p1_adminClientSecret
+  environment_id = var.p1_adminEnvId
+  region         = var.p1_region
+
+  force_delete_production_type = false
+}
+
+resource "pingone_environment" "my_environment" {
+  name        = "New Environment"
+  description = "My new environment"
+  type        = "SANDBOX"
+  region      = "EU"
+  license_id  = var.p1_licenseId
+
+  default_population {
+    name        = "My Population"
+    description = "My new population for users"
+  }
+
+  service {
+    type = "SSO"
+  }
+
+  service {
+    type        = "PingFederate"
+    console_url = "https://my-pingfederate-console.example.com/pingfederate"
+  }
+}
+
+resource "pingone_population" "my_population" {
+  environment_id = pingone_environment.my_environment.id
+
+  name        = "My second population"
+  description = "My new population"
+}
+
+resource "pingone_group" "my_group" {
+  environment_id = pingone_environment.my_environment.id
+
+  name        = "My group"
+  description = "My new group"
+}
---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "p1_adminClientId" {
+  
+}
+
+variable "p1_adminClientSecret" {
+  
+}
+
+variable "p1_adminEnvId" {
+  
+}
+
+variable "p1_region" {
+  
+}
+
+variable "p1_licenseId" {
+  
+}
